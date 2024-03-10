b119639f576375e3764d753b41f1d2061255e50b
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-10 21:18:01 +0530

    lable

--- src/bro/Main.java
+++ src/bro/Main.java
@@ -8,6 +8,15 @@ import java.awt.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        JLabel label = new JLabel();
+        label.setText("Bro, do you even code?");
+
+        JFrame frame = new JFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(500,500);
+        frame.setVisible(true);
 
        /* JFrame frame = new JF rame();
         frame.setTitle("Jframe title goes here");
@@ -20,6 +29,6 @@ public class Main {
         frame.setIconImage(image.getImage());
         frame.getContentPane().setBackground(new Color(0x123456));*/
 
-        MyFrames myFrames = new MyFrames();
+       // MyFrames myFrames = new MyFrames();
     }
 }
