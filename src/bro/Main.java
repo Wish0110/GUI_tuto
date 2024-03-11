8a12a0c5f5461e89519e89bf7a98896f62eff01d
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-11 16:01:05 +0530

    panles margin

--- src/bro/Main.java
+++ src/bro/Main.java
@@ -9,7 +9,7 @@ public class Main {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
-        frame.setLayout(new BorderLayout());
+        frame.setLayout(new BorderLayout(10,10));
         frame.setVisible(true);
 
         JPanel panel1 = new JPanel();
