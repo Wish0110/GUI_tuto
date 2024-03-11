77817fc102402a06342b9c56589b14fc77847f9b
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-11 16:05:42 +0530

    sub panel

--- src/bro/Main.java
+++ src/bro/Main.java
@@ -30,10 +30,33 @@ public class Main {
         panel4.setPreferredSize(new Dimension(100,100));
         panel5.setPreferredSize(new Dimension(100,100));
 
+        //sub panels ----------------
+
+        JPanel panel6 = new JPanel();
+        JPanel panel7 = new JPanel();
+        JPanel panel8 = new JPanel();
+        JPanel panel9 = new JPanel();
+        JPanel panel10 = new JPanel();
+
+        panel1.setBackground(Color.red);
+        panel2.setBackground(Color.green);
+        panel3.setBackground(Color.yellow);
+        panel4.setBackground(Color.magenta);
+        panel5.setBackground(Color.pink);
+
+        panel1.setPreferredSize(new Dimension(100,100));
+        panel2.setPreferredSize(new Dimension(100,100));
+        panel3.setPreferredSize(new Dimension(100,100));
+        panel4.setPreferredSize(new Dimension(100,100));
+        panel5.setPreferredSize(new Dimension(100,100));
+
+        //sub panels ----------------
+
         frame.add(panel1,BorderLayout.NORTH);
         frame.add(panel2,BorderLayout.WEST);
         frame.add(panel3,BorderLayout.EAST);
         frame.add(panel4,BorderLayout.SOUTH);
         frame.add(panel5,BorderLayout.CENTER);
+
     }
 }
