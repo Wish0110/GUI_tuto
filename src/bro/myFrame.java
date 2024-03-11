ad1423f67c0077603ee83473ed880f193c740d7c
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-11 12:30:59 +0530

    button print

--- src/bro/myFrame.java
+++ src/bro/myFrame.java
@@ -4,14 +4,15 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class myFrame extends JFrame implements ActionListener {
+public class myFrame extends JFrame implements ActionListener{
 
     JButton button;
     myFrame(){
 
         button = new JButton();
         button.setBounds(200, 100, 100, 50);
-
+        button.addActionListener((ActionListener) this);
+        button.setText("I'm a Button!");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
