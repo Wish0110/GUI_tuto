b6e32577ca246b6a2d42f9b9350fc3366ed78d63
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-11 16:15:53 +0530

    basics boarder layout

--- src/bro/Main.java
+++ src/bro/Main.java
@@ -24,6 +24,8 @@ public class Main {
         panel4.setBackground(Color.magenta);
         panel5.setBackground(Color.pink);
 
+        panel5.setLayout(new BorderLayout());
+
         panel1.setPreferredSize(new Dimension(100,100));
         panel2.setPreferredSize(new Dimension(100,100));
         panel3.setPreferredSize(new Dimension(100,100));
@@ -38,17 +40,25 @@ public class Main {
         JPanel panel9 = new JPanel();
         JPanel panel10 = new JPanel();
 
-        panel1.setBackground(Color.red);
-        panel2.setBackground(Color.green);
-        panel3.setBackground(Color.yellow);
-        panel4.setBackground(Color.magenta);
-        panel5.setBackground(Color.pink);
+        panel6.setBackground(Color.black);
+        panel7.setBackground(Color.darkGray);
+        panel8.setBackground(Color.gray);
+        panel9.setBackground(Color.lightGray);
+        panel10.setBackground(Color.white);
 
-        panel1.setPreferredSize(new Dimension(100,100));
-        panel2.setPreferredSize(new Dimension(100,100));
-        panel3.setPreferredSize(new Dimension(100,100));
-        panel4.setPreferredSize(new Dimension(100,100));
-        panel5.setPreferredSize(new Dimension(100,100));
+        panel5.setLayout(new BorderLayout());
+
+        panel6.setPreferredSize(new Dimension(50,50));
+        panel7.setPreferredSize(new Dimension(50,50));
+        panel8.setPreferredSize(new Dimension(50,50));
+        panel9.setPreferredSize(new Dimension(50,50));
+        panel10.setPreferredSize(new Dimension(50,50));
+
+        panel5.add(panel6,BorderLayout.NORTH);
+        panel5.add(panel7,BorderLayout.WEST);
+        panel5.add(panel8,BorderLayout.EAST);
+        panel5.add(panel9,BorderLayout.SOUTH);
+        panel5.add(panel10,BorderLayout.CENTER);
 
         //sub panels ----------------
 
