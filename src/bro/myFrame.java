ecacbc96e46202b4441f88f28c7cfe3d4e3912b3
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-11 12:12:49 +0530

    implement button

--- src/bro/myFrame.java
+++ src/bro/myFrame.java
@@ -1,13 +1,28 @@
 package bro;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class myFrame extends JFrame {
+public class myFrame extends JFrame implements ActionListener {
 
+    JButton button;
     myFrame(){
+
+        button = new JButton();
+        button.setBounds(200, 100, 100, 50);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
         this.setVisible(true);
+        this.add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==button) {
+
+        }
     }
 }
