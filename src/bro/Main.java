1f4c40ea15016f49855e59a8c058021bcf033a40
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-11 10:52:55 +0530

    blue panel

--- src/bro/Main.java
+++ src/bro/Main.java
@@ -12,14 +12,18 @@ public class Main {
 
         JPanel redPanel = new JPanel();
         redPanel.setBackground(Color.red);
+        redPanel.setBounds(0,0,250,250);
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250,0,250,250);
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(750,750);
         frame.setLayout(null);
         frame.setVisible(true);
-
-
-
+        frame.add(redPanel);
+        frame.add(bluePanel);
     }
 }
