c989d89773eb98e0a9519bb187e977df31df6303
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-11 19:14:03 +0530

    labl 1 in layerd pane

--- src/bro/Main.java
+++ src/bro/Main.java
@@ -7,6 +7,19 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
 
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.RED);
+        label1.setBounds(50,50,200,200);
 
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0,0,500,500);
+
+        JFrame frame = new JFrame("JLayeredPane");
+        frame.add(layeredPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(new GridLayout(3, 3,10,10));
+        frame.setVisible(true);
     }
 }
