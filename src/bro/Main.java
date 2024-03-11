977131dfaa3bfa5b9f21437ef8690cf5bed9d70f
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-11 12:06:32 +0530

    my frame class add

--- src/bro/Main.java
+++ src/bro/Main.java
@@ -1,47 +1,11 @@
 package bro;
 
-
-import javax.swing.*;
-//import javax.swing.border.Border;
-import java.awt.*;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        ImageIcon icon = new ImageIcon("C:\\Users\\Admin\\Downloads\\icons8-star-filled-48.png");
+       //JButton
 
-        JLabel label = new JLabel();
-        label.setText("Hi");
-        label.setIcon(icon);
-        label.setVerticalAlignment(JLabel.BOTTOM);
-        label.setHorizontalAlignment(JLabel.RIGHT);
-        label.setBounds(0,0,75,75);
-
-        JPanel redPanel = new JPanel();
-        redPanel.setBackground(Color.red);
-        redPanel.setBounds(0,0,250,250);
-        redPanel.setLayout(null);
-
-        JPanel bluePanel = new JPanel();
-        bluePanel.setBackground(Color.blue);
-        bluePanel.setBounds(250,0,250,250);
-
-        JPanel greenPanel = new JPanel();
-        greenPanel.setBackground(Color.green);
-        greenPanel.setBounds(0,250,500,250);
-
-
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(750,750);
-        frame.setLayout(null);
-        frame.setVisible(true);
-        redPanel.add(label);
-        frame.add(redPanel);
-        frame.add(bluePanel);
-        frame.add(greenPanel);
+        new myFrame();
 
     }
 }
---
+++ src/bro/myFrame.java
@@ -0,0 +1,13 @@
+package bro;
+
+import javax.swing.*;
+
+public class myFrame extends JFrame {
+
+    myFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500,500);
+        this.setVisible(true);
+    }
+}
