4d9ef8f7850b55f4ebcef1c36653af50eef7a5f6
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-11 11:56:49 +0530

    rmv lbl

--- src/bro/Main.java
+++ src/bro/Main.java
@@ -15,10 +15,13 @@ public class Main {
         JLabel label = new JLabel();
         label.setText("Hi");
         label.setIcon(icon);
+        label.setVerticalAlignment(JLabel.BOTTOM);
+        label.setHorizontalAlignment(JLabel.RIGHT);
 
         JPanel redPanel = new JPanel();
         redPanel.setBackground(Color.red);
         redPanel.setBounds(0,0,250,250);
+        redPanel.setLayout(null);
 
         JPanel bluePanel = new JPanel();
         bluePanel.setBackground(Color.blue);
@@ -27,6 +30,7 @@ public class Main {
         JPanel greenPanel = new JPanel();
         greenPanel.setBackground(Color.green);
         greenPanel.setBounds(0,250,500,250);
+
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
