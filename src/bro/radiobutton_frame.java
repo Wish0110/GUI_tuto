de6018253c41546d3d29226fb97280e6f26268a2
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-12 20:35:23 +0530

    icon resize

--- src/bro/radiobutton_frame.java
+++ src/bro/radiobutton_frame.java
@@ -10,10 +10,17 @@ public class radiobutton_frame extends JFrame implements ActionListener {
     JRadioButton pizzaButton;
     JRadioButton hamburgerButton;
     JRadioButton hotdogButton;
+    ImageIcon pizzaIcon;
+    ImageIcon hamburgerIcon;
+    ImageIcon hotdogIcon;
 
     radiobutton_frame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+
+        pizzaIcon = resizeIcon(new ImageIcon("C:\\Users\\Admin\\Downloads\\pizza.png"), 30, 30);
+        hamburgerIcon = resizeIcon(new ImageIcon("C:\\Users\\Admin\\Downloads\\hamburger (4).png"), 30, 30);
+        hotdogIcon = resizeIcon(new ImageIcon("C:\\Users\\Admin\\Downloads\\hot-dog.png"), 30, 30);
 
         pizzaButton = new JRadioButton("pizza");
         hamburgerButton = new JRadioButton("hamburger");
@@ -24,16 +31,37 @@ public class radiobutton_frame extends JFrame implements ActionListener {
         group.add(hamburgerButton);
         group.add(hotdogButton);
 
+        pizzaButton.addActionListener(this);
+        hamburgerButton.addActionListener(this);
+        hotdogButton.addActionListener(this);
+
+        pizzaButton.setIcon(pizzaIcon);
+        hamburgerButton.setIcon(hamburgerIcon);
+        hotdogButton.setIcon(hotdogIcon);
+
         this.add(pizzaButton);
         this.add(hamburgerButton);
         this.add(hotdogButton);
         this.pack();
         this.setVisible(true);
     }
+
+    private ImageIcon resizeIcon(ImageIcon icon, int width, int height) {
+        Image image = icon.getImage();
+        Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(scaledImage);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==pizzaButton) {
             System.out.println("You ordered pizza!");
+        }
+        else if(e.getSource()==hamburgerButton) {
+            System.out.println("You ordered hamburger!");
+        }
+        else if(e.getSource()==hotdogButton) {
+            System.out.println("You ordered hotdog!");
         }
     }
 }
