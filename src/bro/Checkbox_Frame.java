c5015a10544f336d3b062e258888615073921a32
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-12 19:55:49 +0530

    icon chng

--- src/bro/Checkbox_Frame.java
+++ src/bro/Checkbox_Frame.java
@@ -15,8 +15,8 @@ public class Checkbox_Frame extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
-        xIcon = new ImageIcon("C:\\Users\\Admin\\Downloads\\OIP (16).jfif");
-        checkIcon = new ImageIcon("C:\\Users\\Admin\\Downloads\\OIP (15).jfif");
+        xIcon = new ImageIcon("C:\\Users\\Admin\\Downloads\\icons8-x-64.png");
+        checkIcon = new ImageIcon("C:\\Users\\Admin\\Downloads\\icons8-check-50.png");
 
         button = new JButton();
         button.setText("submit");
@@ -26,6 +26,7 @@ public class Checkbox_Frame extends JFrame implements ActionListener {
         checkBox.setText("I'am not a robot");
         checkBox.setFocusable(false);
         checkBox.setFont(new Font("Consolas",Font.HANGING_BASELINE,35));
+        checkBox.setIcon(xIcon);
 
         this.add(button);
         this.add(checkBox);
