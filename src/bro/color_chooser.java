0f79e803578fabbf516d7c376f3ab59a2af44033
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-13 15:22:48 +0530

    color_chooser

--- src/bro/color_chooser.java
+++ src/bro/color_chooser.java
@@ -1,4 +1,20 @@
 package bro;
 
-public class color_chooser {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class color_chooser extends JFrame implements ActionListener {
+
+    color_chooser(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.pack();
+        this.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
