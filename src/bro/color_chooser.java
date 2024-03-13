e252264cf4314869d59ced57d586f9d5c4d17897
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-13 15:27:09 +0530

    color_chooser

--- src/bro/color_chooser.java
+++ src/bro/color_chooser.java
@@ -7,9 +7,23 @@ import java.awt.event.ActionListener;
 
 public class color_chooser extends JFrame implements ActionListener {
 
+    JButton button;
+    JLabel label;
+
     color_chooser(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+
+        button = new JButton("Pick a color");
+        button.addActionListener(this);
+
+        label = new JLabel();
+        label.setBackground(Color.white);
+        label.setText("This is some text :D");
+        label.setFont(new Font("MV Boli", Font.PLAIN,100));
+        label.setOpaque(true);
+
+        this.add(button);
         this.pack();
         this.setVisible(true);
     }
