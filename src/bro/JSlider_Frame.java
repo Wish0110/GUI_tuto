1e9b5ef30641f8306adf5a0b9f1dd2dd7b83573f
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-13 10:09:19 +0530

    adjestable slider

--- src/bro/JSlider_Frame.java
+++ src/bro/JSlider_Frame.java
@@ -3,14 +3,28 @@ package bro;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.awt.*;
 
 public class JSlider_Frame implements ChangeListener {
   JFrame frame;
   JPanel panel;
   JLabel label;
+  JSlider slider;
 
     JSlider_Frame(){
 
+        frame = new JFrame("Slider Demo");
+        panel = new JPanel();
+        label = new JLabel();
+        slider = new JSlider(0,100,50);
+
+        slider.setPreferredSize(new Dimension(400,200));
+
+        panel.add(slider);
+        panel.add(label);
+        frame.add(panel);
+        frame.setSize(420,420);
+        frame.setVisible(true);
     }
 
     @Override
